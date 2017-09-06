cc548a297714ad330dbcb8ebee32bcc8a73fc0ff
em-twitch <31691244+em-twitch@users.noreply.github.com>
2017-09-06 11:20:05 +0100

    Update generator.js

--- generator.js
+++ generator.js
@@ -1,3 +1,5 @@
+<script> //
+var $ = jQuery, color2 = $("#colorPicker2").val();
 -$('button').click(function() {
   $('h2').text(randomEl(adjectives) + ' ' + randomEl(nouns));
   selectElementContents($('h2')[0]);
@@ -20,3 +22,4 @@ var adjectives = ["brown", "boisterous", "sweet", "mellow", "long", "sleepy", "s
 var nouns = ["plums", "boooy", "fellow", "boy", "horse", "prince", "wizard", "grumpus", "tippy", "hoop", "hams", "professor", "tip", " lovely son", "brown angel", "gums", "lips", "lip", "face", "hound", "gay", "spicy boy", "meaty hams", "friend", "gay horse"];
 
 $('button').click();
+</script>
